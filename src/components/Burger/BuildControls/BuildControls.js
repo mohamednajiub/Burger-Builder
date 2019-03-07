cde3b51ba5d88feaca4eb9aea8d9125927f40b19
Mohamed Emad <m.emad550@gmail.com>
2019-03-07 15:59:18 +0200

    add more ingreients controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,13 +5,17 @@ const controls = [
     {label: 'Salad', type: 'salad'},
     {label: 'Bacon', type: 'bacon'},
     {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'Meat'},
+    {label: 'Meat', type: 'meat'},
 ];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl Key={ctrl.label} label={ctrl.label} />
+            <BuildControl
+                Key={ctrl.label}
+                label={ctrl.label}
+                added={()=>props.ingredientAdded(ctrl.type)}
+            />
         ))}
     </div>
 );
