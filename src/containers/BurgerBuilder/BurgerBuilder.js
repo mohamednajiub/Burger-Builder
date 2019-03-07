e5007f1c1602cd13773a5a3cd2dc71d25f401233
Mohamed Emad <m.emad550@gmail.com>
2019-03-07 20:39:26 +0200

    finishing Modal functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,6 +91,7 @@ class BurgerBuilder extends Component{
                         ingredients={this.state.ingredients}
                         purchaseCanceled={this.purchaseCancelHandler}
                         purchaseContinued={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}
                     />
                 </Modal>
             }
