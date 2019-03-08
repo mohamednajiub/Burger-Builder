d3a88ae07c5fac138be3c0c2acdc19d8c4d571ce
Mohamed Emad <m.emad550@gmail.com>
2019-03-08 18:11:51 +0200

    fix menu responsive issue

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from '../../assets/images/burger-logo.png'
 import classes from './Logo.module.css';
 const logo = (props)=>(
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={Logo} alt="Burger Builder"/>
     </div>
 );
