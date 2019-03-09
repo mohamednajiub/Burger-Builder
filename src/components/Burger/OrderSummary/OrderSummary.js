1f70929ce2c11b06549c92667e6cfcc3b0d38cd0
Mohamed Emad <m.emad550@gmail.com>
2019-03-09 11:29:22 +0200

    improve app performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey=>{
-        return (
-            <li key={igKey}>
-                <span>{igKey}</span>: {props.ingredients[igKey]}
-            </li>
-        );
-    })
-    return (
-        <>
-            <h3>Your Order</h3>
-            <p>A Delicious Burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price:</strong> {props.price.toFixed(2)} </p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </>
-    )
+class OrderSummary extends Component {
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey=>{
+            return (
+                <li key={igKey}>
+                    <span>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            );
+        })
+        return(
+            <>
+                <h3>Your Order</h3>
+                <p>A Delicious Burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price:</strong> {this.props.price.toFixed(2)} </p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </>
+        )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
