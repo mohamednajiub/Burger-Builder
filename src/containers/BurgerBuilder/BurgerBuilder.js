998c44823b32faaee874e8a52d926abb384730cd
Mohamed Emad <m.emad550@gmail.com>
2019-03-27 14:04:05 +0200

    add spinner to project

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-oders';
+import Spinner from '../../components/UI/Spinner/Spinner'
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -19,7 +21,8 @@ class BurgerBuilder extends Component{
         },
         totalPrice: 4,
         purshasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
     updatePurchaseState(ingredients){
         const sum = Object.keys(ingredients).map(igKey=>{
@@ -74,7 +77,31 @@ class BurgerBuilder extends Component{
         this.setState({purchasing: false})
     }
     purchaseContinueHandler=()=>{
-        alert('You Continue!');
+        // alert('You Continue!');
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Mohamed Emad',
+                email: 'm.emad550@gmail.com',
+                phoneNumber: '01097142156',
+                address: {
+                    street: '34 Marzouq Wahba',
+                    regin: 'Almatrya',
+                    city: 'Cairo',
+                    country: 'Egypt'
+                }
+            }
+        };
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({loading: false, purchasing: false})
+            })
+            .catch(error => {
+                this.setState({loading: false, purchasing: false})
+            })
+        ;
     }
     render (){
         const disableInfo = {
@@ -83,16 +110,20 @@ class BurgerBuilder extends Component{
         for (let key in disableInfo){
             disableInfo[key] = disableInfo[key] <=0
         }
+        let orderSummary = <OrderSummary
+        ingredients={this.state.ingredients}
+        purchaseCanceled={this.purchaseCancelHandler}
+        purchaseContinued={this.purchaseContinueHandler}
+        price={this.state.totalPrice}
+        />;
+        if (this.state.loading){
+            orderSummary = <Spinner/>
+        }
         return(
             <>
             {
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary
-                        ingredients={this.state.ingredients}
-                        purchaseCanceled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}
-                        price={this.state.totalPrice}
-                    />
+                    {orderSummary}
                 </Modal>
             }
                 
