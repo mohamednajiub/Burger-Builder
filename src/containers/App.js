6b11fcdb9d90af463efd2f26dfdc96be3ff44c52
Mohamed Emad <m.emad550@gmail.com>
2019-03-31 12:15:14 +0200

    add checkout page

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Layout from '../hoc/Layout/Layout'
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './Checkout/Checkout';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <BurgerBuilder/>
-          <Checkout />
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Route path="/checkout" exact component={Checkout}/>
         </Layout>
       </div>
     );
