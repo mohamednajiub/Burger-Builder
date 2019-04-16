cd3a359aaac9101304bb91047fe17a6abd0cad4a
Mohamed Emad <m.emad550@gmail.com>
2019-04-16 20:28:49 +0200

    add signup to application using firebase api

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-
+import axios from 'axios';
 export const authStart = () => {
     return {
         type: actionTypes.AUTH_START
@@ -23,5 +23,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch =>  {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCNnVtOtsIis74cBYxstKDUEE_RHaveefA', authData)
+            .then(response=>{
+                console.log(response);
+                dispatch(authSuccess(response.data))
+            })
+            .catch(error=>{
+                console.log(error);
+                dispatch(authFail(error))
+            })
     }
 };
